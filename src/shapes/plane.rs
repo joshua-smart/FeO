b495c350bd240224602ffab01ff803ea8b8ed8b3
joshua-smart <josh@thesmarts.co.uk>
2022-05-29 18:39:49 +0100

    Added u,v coordinate calculations for plane

--- src/shapes/plane.rs
+++ src/shapes/plane.rs
@@ -7,6 +7,8 @@ use crate::shapes::Bounds;
 pub struct Plane {
     position: Vector3,
     normal: Vector3,
+    a_basis: Vector3,
+    b_basis: Vector3,
 
     material_id: usize,
 }
@@ -15,11 +17,15 @@ impl RenderObject for Plane {
     fn intersect(&self, ray: &Ray) -> Option<IntersectionPayload> {
         if self.normal * ray.direction == 0.0 { return None; }
 
-        let distance = (self.position - ray.origin) * self.normal / (self.normal * ray.direction);
+        let distance = ((self.position - ray.origin) * self.normal) / (self.normal * ray.direction);
         if distance <= 0.0 { return None; }
 
         let position = ray.at(distance);
-        Some(IntersectionPayload { position, distance, normal: self.normal.clone(), material_id: self.material_id, u: 0.0, v: 0.0 })
+        let a_dist = (position - self.position) * self.a_basis;
+        let u = if a_dist >= 0.0 { a_dist % 1.0 } else { a_dist % 1.0 + 1.0 };
+        let b_dist = (position - self.position) * self.b_basis;
+        let v = if b_dist >= 0.0 { b_dist % 1.0 } else { b_dist % 1.0 + 1.0 };
+        Some(IntersectionPayload { position, distance, normal: self.normal.clone(), material_id: self.material_id, u, v })
     }
 
     fn bounds(&self) -> Bounds {
@@ -30,6 +36,8 @@ impl RenderObject for Plane {
 impl Plane {
     pub fn new(position: Vector3, normal: Vector3, material_id: usize) -> Plane {
         let unit_normal = normal.normalise();
-        Plane { position, normal: unit_normal, material_id }
+        let b_basis = Vector3::cross(&unit_normal, &Vector3 (1.0, 0.0, 0.0)).normalise();
+        let a_basis = Vector3::cross(&b_basis, &unit_normal);
+        Plane { position, normal: unit_normal, a_basis, b_basis, material_id }
     }
 }

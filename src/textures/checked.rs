323590304b29cafb8554f111a362beaa28a8ce32
joshua-smart <josh@thesmarts.co.uk>
2022-05-29 18:40:06 +0100

    Changed checked texture to respect u,v coordinates

--- src/textures/checked.rs
+++ src/textures/checked.rs
@@ -5,18 +5,16 @@ use crate::maths::Vector3;
 pub struct Checked {
     color_a: Color,
     color_b: Color,
-    scale: f64
 }
 
 impl Texture for Checked {
-    fn value(&self, _u: f64, _v: f64, p: Vector3) -> Color {
-        let sines = (self.scale * p.0).sin() * (self.scale * p.1).sin() * (self.scale * p.2).sin();
-        if sines < 0.0 { self.color_a } else { self.color_b }
+    fn value(&self, u: f64, v: f64, _p: Vector3) -> Color {
+        if ((u - 0.5) * (v - 0.5)) < 0.0 { self.color_a } else { self.color_b }
     }
 }
 
 impl Checked {
-    pub fn new(color_a: Color, color_b: Color, scale: f64) -> Checked {
-        Checked { color_a, color_b, scale }
+    pub fn new(color_a: Color, color_b: Color) -> Checked {
+        Checked { color_a, color_b }
     }
 }
